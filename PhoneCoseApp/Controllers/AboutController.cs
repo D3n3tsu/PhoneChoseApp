40ed658b216ca6b64ae4c099944fc6e0b88c2dab
D3n3tsu <d3n3tsu@gmail.com>
2017-04-26 13:07:30 +0300

    Aboutcontroller configured via atributes

--- PhoneCoseApp/Controllers/AboutController.cs
+++ PhoneCoseApp/Controllers/AboutController.cs
@@ -1,17 +1,23 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+/// <summary>
+/// this controller was made just to test attribute controled routes
+/// </summary>
 namespace PhoneCoseApp.Controllers
 {
+    [Route("[controller]")]
     public class AboutController
     {
+        [Route("{index}")]
         public string Index()
         {
             return "about index";
         }
-
+        [Route("[action]")]
         public string Bout()
         {
             return " about bout";
