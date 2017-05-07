1e448abaf93f03a6f2a67a9c2a599bb412c154a1
D3n3tsu <d3n3tsu@gmail.com>
2017-05-07 23:48:14 +0300

    First migration created.

---
+++ PhoneCoseApp/Migrations/20170507204504_firstCreate.Designer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using PhoneCoseApp.Entities;
+
+namespace PhoneCoseApp.Migrations
+{
+    [DbContext(typeof(PhoneCoseAppDbContext))]
+    [Migration("20170507204504_firstCreate")]
+    partial class firstCreate
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.1")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("PhoneCoseApp.Entities.Phone", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int>("Color");
+
+                    b.Property<string>("Name");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Phones");
+                });
+        }
+    }
+}
---
+++ PhoneCoseApp/Migrations/PhoneCoseAppDbContextModelSnapshot.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using PhoneCoseApp.Entities;
+
+namespace PhoneCoseApp.Migrations
+{
+    [DbContext(typeof(PhoneCoseAppDbContext))]
+    partial class PhoneCoseAppDbContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.1")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("PhoneCoseApp.Entities.Phone", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int>("Color");
+
+                    b.Property<string>("Name");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Phones");
+                });
+        }
+    }
+}
