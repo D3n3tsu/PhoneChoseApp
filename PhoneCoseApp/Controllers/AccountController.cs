9382c380ff12bdda105895f7c46becb5319762b1
D3n3tsu <d3n3tsu@gmail.com>
2017-05-09 15:25:26 +0300

    Registration page created and configured.

---
+++ PhoneCoseApp/Controllers/AccountController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using PhoneCoseApp.Entities;
+using PhoneCoseApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PhoneCoseApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        private UserManager<User> _userManager;
+        private SignInManager<User> _signInManager;
+
+        public AccountController(SignInManager<User> signInManager, UserManager<User> userManeger)
+        {
+            _userManager = userManeger;
+            _signInManager = signInManager;
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterViewModel vm)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = new User { UserName = vm.Username };
+                var createResult = await _userManager.CreateAsync(user, vm.Password);
+                if (createResult.Succeeded) {
+                    await _signInManager.SignInAsync(user, false);
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    foreach(var error in createResult.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+            }
+            return View();
+        }
+    }
+}
